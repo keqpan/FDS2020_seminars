15b5dbb9f3723e49034628cbe82a0596db963be8
keqpan <safinsam@ya.ru>
2020-04-28 10:19:41 +0300

    more refactoring in get_fligts_data()

--- Week 2/Day 3/Submissions/aleksandr_safin/main.py
+++ Week 2/Day 3/Submissions/aleksandr_safin/main.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 import os
 import numpy as np
 import pandas as pd
@@ -9,53 +7,50 @@ import zipfile
 from glob import glob
 
 data_dir = 'data'
+raw_fname = 'nycflights.tar.gz'
+dir_name = 'nycflights'
+url = "https://storage.googleapis.com/dask-tutorial-data/nycflights.tar.gz"
 
 
-def flights():
-    flights_raw = os.path.join(data_dir, 'nycflights.tar.gz')
-    flightdir = os.path.join(data_dir, 'nycflights')
+def get_flights_data(data_dir, raw_fname, dir_name, url):
+    flights_raw = os.path.join(data_dir, raw_fname)
+    flightdir = os.path.join(data_dir, dir_name)
     jsondir = os.path.join(data_dir, 'flightjson')
 
-    if not os.path.exists(data_dir):
-        os.mkdir(data_dir)
+    os.makedirs(data_dir, exist_ok=True)
 
     if not os.path.exists(flights_raw):
         print("- Downloading NYC Flights dataset... ", end='', flush=True)
-        url = "https://storage.googleapis.com/dask-tutorial-data/nycflights.tar.gz"
         urllib.request.urlretrieve(url, flights_raw)
         print("done", flush=True)
 
     if not os.path.exists(flightdir):
         print("- Extracting flight data... ", end='', flush=True)
-        tar_path = os.path.join('data', 'nycflights.tar.gz')
+        tar_path = os.path.join(data_dir, raw_fname)
         with tarfile.open(tar_path, mode='r:gz') as flights:
-            flights.extractall('data/')
+            flights.extractall(data_dir)
         print("done", flush=True)
-
+    
+    n_rows = 10000  # Just take the first 10000 rows for the demo
     if not os.path.exists(jsondir):
         print("- Creating json data... ", end='', flush=True)
         os.mkdir(jsondir)
-        for path in glob(os.path.join('data', 'nycflights', '*.csv')):
+        for path in glob(os.path.join(data_dir, dir_name, '*.csv')):
             prefix = os.path.splitext(os.path.basename(path))[0]
-            # Just take the first 10000 rows for the demo
-            df = pd.read_csv(path).iloc[:10000]
-            df.to_json(os.path.join('data', 'flightjson', prefix + '.json'),
+            
+            df = pd.read_csv(path).iloc[:n_rows]
+            df.to_json(os.path.join(jsondir, prefix + '.json'),
                        orient='records', lines=True)
         print("done", flush=True)
 
-    print("** Finished! **")
 
-
-
-def main():
+def main(args):
     print("Setting up data directory")
     print("-------------------------")
-
-    flights()
-    random_array()
-    weather()
-
-    print('Finished!')
+    get_flights_data(data_dir, raw_fname, dir_name, url)
+    print("** Finished! **")
+    
+    
 
 
 if __name__ == '__main__':
