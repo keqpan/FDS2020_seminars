e18f5c46a53677a5315abc4ba88bb0cb23e0064b
keqpan <safinsam@ya.ru>
2020-04-29 12:56:16 +0300

    cleaning up main.py and distributing logic across other modules

--- Week 2/Day 3/Submissions/aleksandr_safin/main.py
+++ Week 2/Day 3/Submissions/aleksandr_safin/main.py
@@ -1,56 +1,38 @@
 import os
-import numpy as np
+from data_utils import fetch_data
+from data import prepare_data
+from utils import make_cluster
+from dask.distributed import Client
+from sklearn.ensemble import GradientBoostingRegressor
+from dask_ml.xgboost import XGBRegressor
 import pandas as pd
-import tarfile
-import urllib.request
-import zipfile
-from glob import glob
 
-data_dir = 'data'
-raw_fname = 'nycflights.tar.gz'
-dir_name = 'nycflights'
-url = "https://storage.googleapis.com/dask-tutorial-data/nycflights.tar.gz"
-
-
-def get_flights_data(data_dir, raw_fname, dir_name, url):
-    flights_raw = os.path.join(data_dir, raw_fname)
-    flightdir = os.path.join(data_dir, dir_name)
-    jsondir = os.path.join(data_dir, 'flightjson')
-
-    os.makedirs(data_dir, exist_ok=True)
-
-    if not os.path.exists(flights_raw):
-        print("- Downloading NYC Flights dataset... ", end='', flush=True)
-        urllib.request.urlretrieve(url, flights_raw)
-        print("done", flush=True)
-
-    if not os.path.exists(flightdir):
-        print("- Extracting flight data... ", end='', flush=True)
-        tar_path = os.path.join(data_dir, raw_fname)
-        with tarfile.open(tar_path, mode='r:gz') as flights:
-            flights.extractall(data_dir)
-        print("done", flush=True)
-    
-    n_rows = 10000  # Just take the first 10000 rows for the demo
-    if not os.path.exists(jsondir):
-        print("- Creating json data... ", end='', flush=True)
-        os.mkdir(jsondir)
-        for path in glob(os.path.join(data_dir, dir_name, '*.csv')):
-            prefix = os.path.splitext(os.path.basename(path))[0]
-            
-            df = pd.read_csv(path).iloc[:n_rows]
-            df.to_json(os.path.join(jsondir, prefix + '.json'),
-                       orient='records', lines=True)
-        print("done", flush=True)
+param_grid = {"learning_rate": [0.05, 0.1],
+              "n_estimators": [20, 50, 100],
+              "max_depth": [3]}
+from benchmarking import ModelFitter, benchmark_models
 
 
 def main(args):
-    print("Setting up data directory")
-    print("-------------------------")
-    get_flights_data(data_dir, raw_fname, dir_name, url)
-    print("** Finished! **")
+    fetch_data(args.data_dir, args.dataset_name)
     
+    X, y = prepare_data(os.path.join(args.data_dir, args.dataset_name, '*.csv'))
+    X = X[:5000]
+    y = y[:5000]
     
+    print("Starting dask cluster ...")
+    cluster = make_cluster(n_workers=args.n_workers)
+    client = Client()
+    
+    # wrapping our target regressors for further benchmarking
+    sk_xgb = ModelFitter(GradientBoostingRegressor(), True)
+    dask_xgb = ModelFitter(XGBRegressor())
+
+    print("Starting benchmarking process ...")
+    results = benchmark_models(X, y, [sk_xgb, dask_xgb], args.n_folds, param_grid)
+    
+    res_df = pd.DataFrame(index=["sklearn", "dask"], data=results, columns=["GS time", "training time", "Metric"])
+    print(res_df)
 
 
 if __name__ == '__main__':
